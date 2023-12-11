acfb7ce84d5e4e01b8c23875a6ef9dee22ad527b
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2023-12-11 14:46:12 +0530

    Coffee widget

---
+++ src/components/js/coffee.js
@@ -0,0 +1,13 @@
+<script 
+    data-name="BMC-Widget" 
+    data-cfasync="false" 
+    src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" 
+    data-id="bibilicious" 
+    data-description="Support me on Buy me a coffee!" 
+    data-message="Enjoyed the blog? No ads here, just affiliate links and donations to keep it going. Support with a contribution!" 
+    data-color="#40DCA5" 
+    data-position="Right" 
+    data-x_margin="18" 
+    data-y_margin="18"
+    >
+</script>
