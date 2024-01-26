ee46ed0e1799d30a8b44c67d2ed5059328e00965
Samuel Francis <95130256+sicnarfleumas@users.noreply.github.com>
2024-01-26 14:54:09 +0530

    generated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://bibilicious.blog',
-  integrations: [
-    mdx(),
-    sitemap(),
-    react(),
-  ],
-
+  integrations: [mdx(), sitemap(), react()],
   experiments: {
-    api: true,
-  },
+    api: true
+  }
 });
