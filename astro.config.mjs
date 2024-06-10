b2e2d3e70b9a421e5b9d1cfa40a33338f2e15072
Samuel Francis <samuel.francis@learner.manipal.edu>
2024-06-11 00:01:20 +0530

    added db and netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
+import db from "@astrojs/db";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bibilicious.blog',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), db()],
   experiments: {
     api: true
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
